24e152c8f24ca680de9e66bc5f47a63672b7c3be
SirRobZ <sazarapkar@gmail.com>
2017-02-15 14:54:05 -0500

    added recipes endpoint

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const router = express.Router();
 const morgan = require('morgan');
@@ -14,9 +13,9 @@ app.use(morgan('common'));
 
 // we're going to add some items to ShoppingList
 // so there's some data to look at
-ShoppingList.create('beans', 2);
-ShoppingList.create('tomatoes', 3);
-ShoppingList.create('peppers', 4);
+//ShoppingList.create('beans', 2);
+//ShoppingList.create('tomatoes', 3);
+//ShoppingList.create('peppers', 4);
 
 // adding some recipes to `Recipes` so there's something
 // to retrieve.
@@ -27,11 +26,11 @@ Recipes.create(
 
 // when the root of this router is called with GET, return
 // all current ShoppingList items
-app.get('/shopping-list', (req, res) => {
-  res.json(ShoppingList.get());
-});
+//app.get('/shopping-list', (req, res) => {
+//  res.json(ShoppingList.get());
+//});
 
-app.post('/shopping-list', jsonParser, (req, res) => {
+/*app.post('/shopping-list', jsonParser, (req, res) => {
   // ensure `name` and `budget` are in request body
   const requiredFields = ['name', 'budget'];
   for (let i=0; i<requiredFields.length; i++) {
@@ -42,15 +41,32 @@ app.post('/shopping-list', jsonParser, (req, res) => {
       return res.status(400).send(message);
     }
   }
-
-  const item = ShoppingList.create(req.body.name, req.body.budget);
-  res.status(201).json(item);
-});
-
+*/
 
 app.get('/recipes', (req, res) => {
   res.json(Recipes.get());
 })
+
+app.post('/recipes', jsonParser, (req, res) => {
+  const requiredFields = ['name', 'ingredients'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  const item = Recipes.create(req.body.name, req.body.ingredients);
+  res.status(201).json(item);
+});
+
+//  const item = ShoppingList.create(req.body.name, req.body.budget);
+//  res.status(201).json(item);
+//});
+
+
+
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
